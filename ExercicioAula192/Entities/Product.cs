b73a8d60ef8156d13763f77f38036658f63b593a
Cristinabreu <cristinabreu2018@gmail.com>
2021-01-20 23:25:45 +0000

    Inicio do projeto

---
+++ ExercicioAula192/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioAula192.Entities {
+    class Product {
+    }
+}
---
+++ ExercicioAula192/Program.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+
+namespace ExercicioAula192 {
+    class Program {
+        static void Main(string[] args) {   
+
+            Console.WriteLine("Enter file full path: ");
+            string soucrceFilePath = Console.ReadLine();
+
+            
+        }
+    }
+}
