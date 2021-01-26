ba1e31093bb9b4421f37c1caa2f78da7eebf6d0b
Cristinabreu <cristinabreu2018@gmail.com>
2021-01-26 15:50:09 +0000

    Programa para ler o caminho de um arquivo

--- ExercicioAula192/Program.cs
+++ ExercicioAula192/Program.cs
@@ -6,12 +6,39 @@ using System.IO;
 
 namespace ExercicioAula192 {
     class Program {
-        static void Main(string[] args) {   
+        static void Main(string[] args) {
 
             Console.WriteLine("Enter file full path: ");
-            string soucrceFilePath = Console.ReadLine();
+            string sourceFilePath = Console.ReadLine();
 
-            
+            try {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(sourceFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath)){
+                    foreach(string line in lines) {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quatity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quatity);
+
+                        sw.WriteLine(product.Name + "," + product.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
